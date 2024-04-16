eb90e29c30cefb204f4170b75e8f59d34cb60a77
hojoon0724 <hojoon0724@gmail.com>
2024-04-15 22:59:08 -0700

    Initial Commit

---
+++ BirthdaysTutorial/BirthdaysTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysTutorialApp.swift
+//  BirthdaysTutorial
+//
+//  Created by Hojoon Kim on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
